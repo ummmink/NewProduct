3b4ef1fc841fefd7ff3fd44756fea91bab1a2512
Mink <n.sarawana@gmail.com>
2018-10-02 14:31:46 +0700

    02/10/2018_14:31

--- NewProduct/Properties/Resources.Designer.cs
+++ NewProduct/Properties/Resources.Designer.cs
@@ -103,16 +103,6 @@ namespace NewProduct.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap recycling_day_calendar_reminder_page {
-            get {
-                object obj = ResourceManager.GetObject("recycling_day_calendar_reminder_page", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap repeat {
             get {
                 object obj = ResourceManager.GetObject("repeat", resourceCulture);
